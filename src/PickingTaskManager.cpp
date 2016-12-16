7051f469a23aacf85de6b6d9ad042bdfc4b01248
ogata <futail2a@gmail.com>
2016-12-16 15:18:17 +0900

    first commit

---
+++ src/PickingTaskManager.cpp
@@ -0,0 +1,178 @@
+// -*- C++ -*-
+/*!
+ * @file  PickingTaskManager.cpp
+ * @brief ModuleDescription
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "PickingTaskManager.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* pickingtaskmanager_spec[] =
+  {
+    "implementation_id", "PickingTaskManager",
+    "type_name",         "PickingTaskManager",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "ogata-lab",
+    "category",          "Category",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+PickingTaskManager::PickingTaskManager(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_ObjectDetectionServicePort("ObjectDetectionService"),
+    m_ManipulationPlannerServicePort("ManipulationPlannerService"),
+    m_KinematicsSolverServicePort("KinematicsSolverService"),
+    m_MotionGeneratorServicePort("MotionGeneratorService")
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+PickingTaskManager::~PickingTaskManager()
+{
+}
+
+
+
+RTC::ReturnCode_t PickingTaskManager::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  
+  // Set OutPort buffer
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  m_ObjectDetectionServicePort.registerConsumer("ObjectDetectionService", "Manipulation::ObjectDetectionService", m_ObjectDetectionService);
+  m_ManipulationPlannerServicePort.registerConsumer("ManipulationPlannerService", "Manipulation::ManipulationPlannerService", m_ManipulationPlannerService);
+  m_KinematicsSolverServicePort.registerConsumer("KinematicsSolverService", "Manipulation::KinematicsSolverService", m_KinematicsSolverService);
+  m_MotionGeneratorServicePort.registerConsumer("MotionGeneratorService", "Manipulation::MotionGeneratorService", m_MotionGeneratorService);
+  
+  // Set CORBA Service Ports
+  addPort(m_ObjectDetectionServicePort);
+  addPort(m_ManipulationPlannerServicePort);
+  addPort(m_KinematicsSolverServicePort);
+  addPort(m_MotionGeneratorServicePort);
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onExecute(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t PickingTaskManager::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void PickingTaskManagerInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(pickingtaskmanager_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<PickingTaskManager>,
+                             RTC::Delete<PickingTaskManager>);
+  }
+  
+};
+
+
