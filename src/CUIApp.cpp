1a5309c1a188e2b661260ebf2dc8f4f2c43b4c43
tao <futail2a@gmail.com>
2016-12-18 14:12:56 +0900

    impl CUIApp

--- src/CUIApp.cpp
+++ src/CUIApp.cpp
@@ -1,18 +1,86 @@
 #include <CUIApp.h>
-	(*m_objectID)
-	m_objInfo
-	m_goalRobotJointInfo);
 
-	m_MotionGeneratorService->getCurrentRobotJointInfo((*m_robotID), m_robotJoint);
-	m_ManipulationPlannerService->planManipulation((*m_robotID), (*m_startRobotJointInfo), (*m_goalRobotJointInfo), m_manipPlan);
-	break;
+CUIApp::CUIApp(PickingTaskManager* compPtr)
+{
+	m_objectID = new Manipulation::ObjectIdentifier();
+	m_objInfo = new Manipulation::ObjectInfo();
+
+	m_robotID = new Manipulation::RobotIdentifier();
+	m_robotJoint = new Manipulation::RobotJointInfo();
+
+	m_startRobotJointInfo = new Manipulation::RobotJointInfo();
+	m_goalRobotJointInfo = new Manipulation::RobotJointInfo();
+	m_manipPlan = new Manipulation::ManipulationPlan();
+
+	m_rtc = compPtr;
+}
+
+void CUIApp::detectObj(){
+	std::cout << "--Object Detection--" << std::endl;
+	std::cout << "Please input target object name" << std::endl;
+	char* name;
+	std::cin >> name;
+	(*m_objectID).name = name;
+
+	m_rtc->callDetectObject((*m_objectID), m_objInfo);
+}
+
+void CUIApp::searchMotionPlan(){
+	std::cout << "--Motion Plannig--" << std::endl;
+
+	std::cout << "Solve grasping position" << std::endl;
+	m_rtc->callSolveInverseKinematics((*m_objInfo), m_goalRobotJointInfo);
+
+	m_rtc->callGetCurrentRobotJointInfo((*m_robotID), m_robotJoint);
+	m_rtc->callPlanManipulation((*m_robotID), (*m_startRobotJointInfo), (*m_goalRobotJointInfo), m_manipPlan);
 
 
-	void showManipPlan(){
-		for (int i = 0; i<m_manipPlan->robotJointInfoSeq.length(); i++){
-			for (int j = 0; j<m_manipPlan->robotJointInfoSeq[i].jointInfoSeq.length(); j++){
-				std::cout << m_manipPlan->robotJointInfoSeq[i].jointInfoSeq[j].jointAngle << " ";
-			}
-		std::cout << std::endl;
+}
+
+void CUIApp::generateMotionPlan(){
+	std::cout << "--Motion Generation--" << std::endl;
+	m_rtc->callFollowManipPlan((*m_manipPlan));
+
+}
+
+void CUIApp::showParams(){
+	for (int i = 0; i<m_manipPlan->robotJointInfoSeq.length(); i++){
+		for (int j = 0; j<m_manipPlan->robotJointInfoSeq[i].jointInfoSeq.length(); j++){
+			std::cout << m_manipPlan->robotJointInfoSeq[i].jointInfoSeq[j].jointAngle << " ";
 		}
+	std::cout << std::endl;
 	}
+}
+
+
+//for debugging only, delete this function before release
+void CUIApp::setSampleManipPlan(){
+	std::string str;
+	std::ifstream ifs("sampleManipPath.csv");
+
+	if (!ifs){
+		std::cout << "error" << std::endl;
+	}
+
+	while (getline(ifs, str))
+	{
+		std::string tmp;
+		std::istringstream stream(str);
+		Manipulation::RobotJointInfo posture;
+
+		while (getline(stream, tmp, ','))
+		{
+			Manipulation::JointInfo joint;
+			joint.jointAngle = std::stod(tmp);
+
+			CORBA::ULong len = posture.jointInfoSeq.length();
+			posture.jointInfoSeq.length(len + 1);
+			posture.jointInfoSeq[len] = joint;
+		}
+
+		CORBA::ULong len = m_manipPlan->robotJointInfoSeq.length();
+		m_manipPlan->robotJointInfoSeq.length(len + 1);
+		m_manipPlan->robotJointInfoSeq[len] = posture;
+	}
+
+}
--- src/PickingTaskManager.cpp
+++ src/PickingTaskManager.cpp
@@ -104,22 +104,10 @@ RTC::ReturnCode_t PickingTaskManager::onShutdown(RTC::UniqueId ec_id)
   return RTC::RTC_OK;
 }
 */
-void PickingTaskManager::initParams()
-{
-	m_objectID = new Manipulation::ObjectIdentifier();
-	m_objInfo = new Manipulation::ObjectInfo();
-
-	m_robotID = new Manipulation::RobotIdentifier();
-	m_robotJoint = new Manipulation::RobotJointInfo();
-
-	m_startRobotJointInfo = new Manipulation::RobotJointInfo();
-	m_goalRobotJointInfo = new Manipulation::RobotJointInfo();
-	m_manipPlan = new Manipulation::ManipulationPlan();
-}
 
 RTC::ReturnCode_t PickingTaskManager::onActivated(RTC::UniqueId ec_id)
 {
-  initParams();
+  m_app = new CUIApp(this);
   return RTC::RTC_OK;
 }
 
@@ -140,6 +128,29 @@ RTC::ReturnCode_t PickingTaskManager::onExecute(RTC::UniqueId ec_id)
 
   switch(c){
   
+  case 'd':
+	  m_app->detectObj();
+	  break;
+
+  case 'p':
+	  m_app->searchMotionPlan();
+	  break;
+
+  case 'g':
+	  m_app->generateMotionPlan();
+	  break;
+
+  case 'e':
+	  std::cout << "Generate motion from a csv file" << std::endl;
+	  m_app->setSampleManipPlan();
+	  //std::cout << "Test solving kinematics.." << std::endl;
+	  //m_KinematicsSolverService->solveInverseKinematics((*m_objInfo), m_goalRobotJointInfo);
+	  break;
+
+  case's':
+	  m_app->showParams();
+	  break;
+	  
   case 'h':
 	  std::cout << "d: detect target object" << std::endl;
 	  std::cout << "p: search motion plan" << std::endl;
@@ -148,47 +159,7 @@ RTC::ReturnCode_t PickingTaskManager::onExecute(RTC::UniqueId ec_id)
 
 	  std::cout << "h: help" << std::endl;
 	  std::cout << "e: debug mode" << std::endl;
-	break;
-  
-  case 'd':
-	  std::cout << "--Object Detection--" << std::endl;
-	  std::cout << "Please input target object name" << std::endl;
-	  char* name;
-	  std::cin >> name;
-	  (*m_objectID).name = name;
-
-	  m_ObjectDetectionService->detectObject((*m_objectID), m_objInfo);
 	  break;
-
-  case 'p':
-	  std::cout << "--Motion Plannig--" << std::endl;
-
-	  std::cout << "Solve grasping position" << std::endl;
-	  m_KinematicsSolverService->solveInverseKinematics((*m_objInfo), m_goalRobotJointInfo);
-
-	  m_MotionGeneratorService->getCurrentRobotJointInfo((*m_robotID), m_robotJoint);
-	  m_ManipulationPlannerService->planManipulation((*m_robotID), (*m_startRobotJointInfo), (*m_goalRobotJointInfo), m_manipPlan);
-	  break;
-
-  case 'g':
-	  std::cout << "--Motion Generation--" << std::endl;
-	  m_MotionGeneratorService->followManipPlan((*m_manipPlan));
-	  break;
-
-  case 'e':
-	  std::cout << "generate motion from csv file" << std::endl;
-	  setSampleManipPlan();
-	  //std::cout << "Test solving kinematics.." << std::endl;
-	  //m_KinematicsSolverService->solveInverseKinematics((*m_objInfo), m_goalRobotJointInfo);
-	  break;
-
-  case's':
-	  for(int i =0;i<m_manipPlan->robotJointInfoSeq.length(); i++){
-		  for (int j = 0; j<m_manipPlan->robotJointInfoSeq[i].jointInfoSeq.length(); j++){
-			  std::cout << m_manipPlan->robotJointInfoSeq[i].jointInfoSeq[j].jointAngle << " ";
-		  }
-		  std::cout <<std::endl;
-	  }
   }
 
   return RTC::RTC_OK;
@@ -229,7 +200,25 @@ RTC::ReturnCode_t PickingTaskManager::onRateChanged(RTC::UniqueId ec_id)
 }
 */
 
+void PickingTaskManager::callDetectObject(const Manipulation::ObjectIdentifier& objectID, Manipulation::ObjectInfo_out objInfo){
+	m_ObjectDetectionService->detectObject(objectID, objInfo);
+}
 
+void PickingTaskManager::callSolveInverseKinematics(const Manipulation::ObjectInfo& objInfo, Manipulation::RobotJointInfo_out goalRobotJointInfo){
+	m_KinematicsSolverService->solveInverseKinematics(objInfo, goalRobotJointInfo);
+}
+
+void PickingTaskManager::callGetCurrentRobotJointInfo(const Manipulation::RobotIdentifier& robotID, Manipulation::RobotJointInfo_out robotJoint){
+	m_MotionGeneratorService->getCurrentRobotJointInfo(robotID, robotJoint);
+}
+
+void PickingTaskManager::callPlanManipulation(const Manipulation::RobotIdentifier& robotID, const Manipulation::RobotJointInfo& startRobotJointInfo, const Manipulation::RobotJointInfo& goalRobotJointInfo, Manipulation::ManipulationPlan_out manipPlan){
+	m_ManipulationPlannerService->planManipulation(robotID, startRobotJointInfo, goalRobotJointInfo, manipPlan);
+}
+
+void PickingTaskManager::callFollowManipPlan(const Manipulation::ManipulationPlan& manipPlan){
+	m_MotionGeneratorService->followManipPlan(manipPlan);
+}
 
 extern "C"
 {
@@ -243,37 +232,3 @@ extern "C"
   }
   
 };
-
-
-
-//for debugging only, delete this function before release
-void PickingTaskManager::setSampleManipPlan(){
-    std::string str;
-	std::ifstream ifs("sampleManipPath.csv");
-
-    if (!ifs){
-        std::cout << "error" << std::endl;
-    }
-
-    while(getline(ifs,str))
-    {
-        std::string tmp;
-        std::istringstream stream(str);
-    	Manipulation::RobotJointInfo posture;
-
-        while(getline(stream,tmp,','))
-        {
-        	Manipulation::JointInfo joint;
-        	joint.jointAngle = std::stod(tmp);
-
-        	CORBA::ULong len = posture.jointInfoSeq.length();
-            posture.jointInfoSeq.length(len + 1);
-            posture.jointInfoSeq[len] = joint;
-    	}
-
-        CORBA::ULong len = m_manipPlan->robotJointInfoSeq.length();
-        m_manipPlan->robotJointInfoSeq.length(len + 1);
-	    m_manipPlan->robotJointInfoSeq[len]=posture;
-    }
-
-}
